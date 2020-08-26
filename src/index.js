90eb35451448d4dab4355d902542136927f297b8
George Koniaris <koniaris.giwrgos@gmail.com>
2020-08-26 14:42:36 +0300

    Redirect to profile if a point has already been marked as circle or mark

--- src/index.js
+++ src/index.js
@@ -33,9 +33,15 @@ app.get('/getPoint/:x/:y', (req, res) => {
 app.get('/setPoint/:x/:y', (req, res) => {
   res.header("Access-Control-Allow-Origin", "*");
 
+  if (game[req.params.y][req.params.x] === 'mark' || game[req.params.y][req.params.x] === 'circle') {
+    return res.redirect('https://github.com/gkoniaris/gkoniaris')
+  }
+
   game[req.params.y][req.params.x] = turn === 0 ? 'mark' : 'circle'
 
-  return res.redirect('back')
+  turn = turn === 1 ? 0 : 1
+
+  return res.redirect('https://github.com/gkoniaris/gkoniaris')
 })
 
 app.listen(port, () => {
